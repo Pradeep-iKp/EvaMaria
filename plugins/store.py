9ae899b161ddc180746994e769851c3937505b6e
P̤radeep <78857587+Pradeep-iKp@users.noreply.github.com>
2022-06-27 20:38:53 +0530

    Update store.py

--- plugins/store.py
+++ plugins/store.py
@@ -3,11 +3,14 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 # Configs
-API_HASH = os.environ['API_HASH']
-APP_ID = int(os.environ['APP_ID'])
-BOT_TOKEN = os.environ['BOT_TOKEN']
-TRACK_CHANNEL = int(os.environ['TRACK_CHANNEL'])
-OWNER_ID = os.environ['OWNER_ID']
+#API_HASH = os.environ['API_HASH']
+#APP_ID = int(os.environ['APP_ID'])
+#BOT_TOKEN = os.environ['BOT_TOKEN']
+#TRACK_CHANNEL = int(os.environ['TRACK_CHANNEL'])
+#OWNER_ID = os.environ['OWNER_ID']
+
+
+OWNER_ID = 648705558
 
 #Button
 START_BUTTONS=[
@@ -19,16 +22,16 @@ START_BUTTONS=[
 ]
 
 # Running bot
-xbot = Client('File-Sharing', api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+# xbot = Client('File-Sharing', api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 # Notify about bot start
-with xbot:
-    xbot_username = xbot.get_me().username  # Better call it global once due to telegram flood id
-    print("Bot started!")
-    xbot.send_message(int(OWNER_ID), "Bot started!")
+# with xbot:
+  #  xbot_username = xbot.get_me().username  # Better call it global once due to telegram flood id
+   # print("Bot started!")
+  #  xbot.send_message(int(OWNER_ID), "Bot started!")
 
 
 # Start & Get file
-@xbot.on_message(filters.command('start') & filters.private)
+@Client.on_message(filters.command('start') & filters.private)
 async def _startfile(bot, update):
     if update.text == '/start':
         await update.reply_text(
@@ -47,10 +50,10 @@ async def _startfile(bot, update):
         if not msg_id.isdigit():
             return
         try:  # If message not belong to media group raise exception
-            check_media_group = await bot.get_media_group(TRACK_CHANNEL, int(msg_id))
+            check_media_group = await bot.get_media_group(LOG_CHANNEL, int(msg_id))
             check = check_media_group[0]  # Because func return`s list obj
         except Exception:
-            check = await bot.get_messages(TRACK_CHANNEL, int(msg_id))
+            check = await bot.get_messages(LOG_CHANNEL, int(msg_id))
 
         if check.empty:
             await update.reply_text('Error: [Message does not exist]\n/help for more details...')
@@ -74,7 +77,7 @@ async def _startfile(bot, update):
         if unique_id != unique_idx.lower():
             return
         try:  # If message not belong to media group raise exception
-            await bot.copy_media_group(update.from_user.id, TRACK_CHANNEL, int(msg_id))
+            await bot.copy_media_group(update.from_user.id, LOG_CHANNEL, int(msg_id))
         except Exception:
             await check.copy(update.from_user.id)
     else:
@@ -121,7 +124,7 @@ async def __reply(update, copied):
 
 # Store media_group
 media_group_id = 0
-@xbot.on_message(filters.media & filters.private & filters.media_group)
+@Client.on_message(filters.media & filters.private & filters.media_group)
 async def _main_grop(bot, update):
     global media_group_id
     if OWNER_ID == 'all':
@@ -133,7 +136,7 @@ async def _main_grop(bot, update):
 
     if int(media_group_id) != int(update.media_group_id):
         media_group_id = update.media_group_id
-        copied = (await bot.copy_media_group(TRACK_CHANNEL, update.from_user.id, update.message_id))[0]
+        copied = (await bot.copy_media_group(LOG_CHANNEL, update.from_user.id, update.message_id))[0]
         await __reply(update, copied)
 
     else:
@@ -143,7 +146,7 @@ async def _main_grop(bot, update):
 
 
 # Store file
-@xbot.on_message(filters.media & filters.private & ~filters.media_group)
+@Client.on_message(filters.media & filters.private & ~filters.media_group)
 async def _main(bot, update):
     if OWNER_ID == 'all':
         pass
@@ -152,8 +155,8 @@ async def _main(bot, update):
     else:
         return
 
-    copied = await update.copy(TRACK_CHANNEL)
+    copied = await update.copy(LOG_CHANNEL)
     await __reply(update, copied)
 
 
-xbot.run()
+
